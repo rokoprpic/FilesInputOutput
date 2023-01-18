a2c4fa0c824feede2e7200d6d7131ac4eb5042c4
Roko Prpić <roko.prpic@gmail.com>
2023-01-18 14:59:32 +0100

    Add project files.

---
+++ ReadFileDisplayLongestWord/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ReadFileDisplayLongestWord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = "text.txt";
+            try
+            {
+                var words = File.ReadAllText(path).Split(' ');
+                var max = words[0];
+                foreach (var word in words)
+                {
+                    if(word.Length>max.Length)
+                        max = word;
+                }
+                Console.WriteLine(max);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("error");
+            }
+        }
+    }
+}
---
+++ ReadFileDisplayN/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ReadFileDisplayN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = "text.txt";
+            try
+            {
+                Console.WriteLine(File.ReadAllText(path).Split(' ').Count());
+            } catch(Exception ex)
+            {
+                Console.WriteLine("error");
+            }
+        }
+    }
+}
